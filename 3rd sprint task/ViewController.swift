fcf0854ba0a31953a40d0eb8592dcf7e112eda98
Rustam Sadykov <sadikov91@gmail.com>
2023-07-02 16:02:04 +0500

    Initial Commit

---
+++ 3rd sprint task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3rd sprint task
+//
+//  Created by Rustam Sadykov on 02.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
